cca6b4c94bfa5a70618eb1dfe08a13b248469fcb
Kalyan <kalyan11021980@gmail.com>
2019-03-10 16:34:39 -0400

    added lazy loaded support

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 import { InjectorServiceModule } from './../../src/scriptinject/injector.module';
 import { InjectorService } from './../../src/scriptinject/inject';
+import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 
 export function onBootstrap(appRef: ApplicationRef, scriptinject: InjectorService) {
   return () => {
@@ -20,7 +21,8 @@ export function onBootstrap(appRef: ApplicationRef, scriptinject: InjectorServic
     AppModule,
     ServerModule,
     ServerTransferStateModule,
-    InjectorServiceModule
+    InjectorServiceModule,
+    ModuleMapLoaderModule
   ],
   bootstrap: [AppComponent],
   providers: [
